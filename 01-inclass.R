30c3588d1dd87d220a39fa069e5d6ac6c79ec6ed
Jie <jiejiao404@gmail.com>
2026-09-21 11:11:40 -0400

    comit

--- 01-inclass.R
+++ 01-inclass.R
@@ -64,19 +64,3 @@ aggregate(xxx) |>
 
 # Push your completed code to your GitHub repository
 
-
-#set name 
-install.packages("usethis")
-library(usethis) 
-
-usethis::use_git_config(user.name = "Jie-jiao05", user.email = "jiejiao404@gmail.com")
-
-# to confirm, generate a git situation-report, your user name and email 
-# should appear under Git config (global)
-usethis::git_sitrep()
-
-
-
-
-
-
